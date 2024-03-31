d2b2480c93cfb15b329b812ca5ad8713033f60d0
Sabrrii <sabri.rabahia@sts-sio-caen.info>
2024-03-31 16:17:09 +0200

    les carte dans ma collection s'affiche ->en utilisant les carte dans la BDD, design et pos a travailler

--- app/src/main/java/com/example/yugivault/CardCollection.kt
+++ app/src/main/java/com/example/yugivault/CardCollection.kt
@@ -25,22 +25,6 @@ class CardCollection : ComponentActivity() {
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        val carrdList = listOf(
-            Card(1,"Dark Magician", "Spellcaster", "The ultimate wizard in terms of attack and defense.", 2500, 2100, 7, "Dark", "Normal","","","",""),
-            Card(2,"Blue-Eyes White Dragon", "Dragon", "This legendary dragon is a powerful engine of destruction.", 3000, 2500, 8, "Light", "Normal","","","",""),
-            Card(3,"Red-Eyes Black Dragon", "Dragon", "A ferocious dragon with a deadly attack.", 2400, 2000, 7, "Dark", "Normal","","","","")
-        )
-
-        println("Taille de la liste: ${carrdList.size}")
-        adapter = CardAdapter(carrdList)
-        recyclerView.adapter = adapter
-
-/*        val Name = findViewById<TextView>(R.id.Name)
-        val Attribute = findViewById<TextView>(R.id.Attribute)
-        val ArtWork = findViewById<ImageView>(R.id.ArtWork)
-        val TypeRace = findViewById<TextView>(R.id.TypeRace)
-        val AtkDef = findViewById<TextView>(R.id.AtkDef)*/
-
 
         val db = Room.databaseBuilder(
             applicationContext,
@@ -50,17 +34,16 @@ class CardCollection : ComponentActivity() {
         val cardDAO = db.cardDAO()
 
 
-        //GlobalScope.launch{
-/*            val cards = cardDAO.getCards()
-            println(cards.value.toString())
-            cards.observe(this,{cards ->
-                cards?.forEach(){
-                    println("Card: ${it.name}, Type: ${it.type},desc: ${it.desc},id: ${it.uid}")
-                    Name.text = it.name
-                    Attribute.text = it.attribute
+        val cards = cardDAO.getCards()
+        println(cards.value.toString())
+        cards.observe(this,{cards ->
+            cards?.let{
+                if (it.isNotEmpty()){
+                    adapter = CardAdapter(it)
+                    recyclerView.adapter = adapter
                 }
-            })*/
-       // }
+            }
+        })
 
     }
 
